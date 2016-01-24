a594aab7bd63c5f3fa9c7f77bc8b56aa24c93f4a
James Monger <jameskmonger@hotmail.co.uk>
2016-01-24 22:27:34 +0000

    add gulpfile.ts

---
+++ gulpfile.ts
@@ -0,0 +1,18 @@
+/// <reference path="typings/gulp/gulp.d.ts" />
+
+import * as gulp from 'gulp';
+
+class GulpEnvironment {
+  constructor() {
+
+  }
+
+  public registerTasks(): void {
+
+  }
+}
+
+(function() {
+  var g = new GulpEnvironment();
+  g.registerTasks();
+})();
