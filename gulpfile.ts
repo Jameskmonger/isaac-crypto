740e8e050d6241b0287fa0d3f55876e2fa44ec9a
James Monger <jameskmonger@hotmail.co.uk>
2016-01-25 13:52:05 +0000

    Build tests with gulp

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,15 +1,17 @@
 /// <reference path="typings/gulp/gulp.d.ts" />
 
 import * as gulp from 'gulp';
+const tape = require('gulp-tape');
+const tsc = require('gulp-tsc');
 
 class GulpEnvironment {
-  constructor() {
-
-  }
+  constructor() { }
 
   public registerTasks(): void {
-    gulp.task('bla', () => {
-      console.log("bla bla bla");
+    gulp.task('build:test', () => {
+      gulp.src(['test/**/*.ts'])
+        .pipe(tsc())
+        .pipe(gulp.dest('build-test/'))
     });
   }
 }
