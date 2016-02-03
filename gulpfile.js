0dee414e178090a946699dacffbff1fdd5abccbf
James Monger <jameskmonger@hotmail.co.uk>
2016-02-03 23:35:08 +0000

    gulp test task complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp');
 var tsc = require('gulp-tsc');
+var tape = require('gulp-tape');
+var tapSpec = require('tap-spec');
 
 gulp.task("build:test", function (done) {
   gulp.src(['test/**/*.ts'])
     .pipe(tsc())
     .pipe(gulp.dest('build-test/'))
     .on('end', done);
+});
+
+gulp.task("test", ["build:test"], function (done) {
+  gulp.src('build-test/test/*.test.js')
+    .pipe(tape({
+      reporter: tapSpec()
+    }));
 });
