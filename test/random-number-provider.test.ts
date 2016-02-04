f2b00cc265de1e8a0281d639565e87e9a12f30e8
James Monger <jameskmonger@hotmail.co.uk>
2016-02-04 23:55:33 +0000

    Rename random-number.provider to random-number-provider

--- test/random-number-provider.test.ts
+++ test/random-number-provider.test.ts
@@ -1,7 +1,7 @@
 /// <reference path="../typings/tape/tape.d.ts"/>
 
 import * as test from 'tape';
-import { RandomNumberProvider } from '../src/random-number.provider';
+import { RandomNumberProvider } from '../src/random-number-provider';
 
 test('it calls to Math.random', (t) => {
   t.plan(1);
