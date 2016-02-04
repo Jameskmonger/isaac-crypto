151c8bcbaddeb66f83e09409767bd2aedc8afc85
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-02-04 13:58:57 +0000

    Add (failing) MSB tests

--- src/bit-operations.ts
+++ src/bit-operations.ts
@@ -2,4 +2,8 @@ export class BitOperations {
   public static getLeastSignificantBit(input: number): number {
     return input & 0b0001;
   }
+
+  public static getMostSignificantBit(input: number): number {
+    return 5;
+  }
 }
