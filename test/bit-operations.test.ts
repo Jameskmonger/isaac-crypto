ee14f5c00b3cb0201e51ad70a2681f244e3fc2a1
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-02-04 13:42:01 +0000

    Test most significant bit

--- test/bit-operations.test.ts
+++ test/bit-operations.test.ts
@@ -16,3 +16,17 @@ for (let testCase of [[0b0001, 1], [0b0010, 0], [0b0101, 1], [0b1010, 0],
     [0b0111, 1], [0b1111, 1], [0b0000, 0], [0b10100111, 1]]) {
   testLeastSignificantBit(testCase[0], testCase[1]);
 }
+
+let testMostSignificantBit = (input: number, output: number) => {
+  test(`it gets the correct most significant bit [TestCase: ${input}]`, (t) => {
+    let lsb = BitOperations.getMostSignificantBit(input);
+
+    t.equal(lsb, output);
+    t.end();
+  });
+};
+
+for (let testCase of [[0b0001, 0], [0b0010, 0], [0b0101, 0], [0b1010, 1],
+    [0b0111, 0], [0b1111, 1], [0b0000, 0], [0b10100111, 1]]) {
+  testMostSignificantBit(testCase[0], testCase[1]);
+}
