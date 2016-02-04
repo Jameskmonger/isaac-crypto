b3356ff03e4284af36aa5557b5ad4f239a2b8612
James Monger <jameskmonger@hotmail.co.uk>
2016-02-04 23:52:55 +0000

    Add tape plan feature

--- test/random-number.provider.test.ts
+++ test/random-number.provider.test.ts
@@ -4,18 +4,16 @@ import * as test from 'tape';
 import { RandomNumberProvider } from '../src/random-number.provider';
 
 test('it calls to Math.random', (t) => {
+  t.plan(1);
+
   let n = new RandomNumberProvider();
 
   let mathDotRandomCalled = false;
 
   Math.random = () => {
-    mathDotRandomCalled = true;
-
+    t.pass();
     return 1;
   };
 
   n.getRandom();
-
-  t.true(mathDotRandomCalled, "Math.Random was called");
-  t.end();
 });
