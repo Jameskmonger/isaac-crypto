ae3c4c5ddc569645c2c5eca760c50bc09a37f0a4
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-02-04 13:48:04 +0000

    Remove MSB tests

--- src/bit-operations.ts
+++ src/bit-operations.ts
@@ -2,8 +2,4 @@ export class BitOperations {
   public static getLeastSignificantBit(input: number): number {
     return input & 0b1;
   }
-
-  public static getMostSignificantBit(input: number): number {
-    return 5;
-  }
 }
