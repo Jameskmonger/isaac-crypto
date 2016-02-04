a2c01156a1ca603c57d6a0fbcfd9fab3970445f5
James Monger <jameskmonger@hotmail.co.uk>
2016-02-04 23:58:14 +0000

    Add test for regeneration

--- test/isaac-generator.test.ts
+++ test/isaac-generator.test.ts
@@ -2,3 +2,16 @@
 
 import * as test from 'tape';
 import { IsaacGenerator } from '../src/isaac-generator';
+
+test("getValue calls _randomise if count is 0", (t) => {
+  let generator = new IsaacGenerator();
+  generator["_count"] = 0;
+
+  let _randomiseCalled = false;
+  generator["_randomise"] = () => {
+    _randomiseCalled = true;
+  }
+
+  t.true(_randomiseCalled, "_randomise called");
+  t.end();
+});
