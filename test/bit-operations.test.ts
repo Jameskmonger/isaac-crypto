2084c753820a4a478d5e00dfb08169d7365434b9
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-02-04 13:33:26 +0000

    Add more test cases to LSB

--- test/bit-operations.test.ts
+++ test/bit-operations.test.ts
@@ -12,6 +12,7 @@ let testLeastSignificantBit = (input: number, output: number) => {
   });
 };
 
-for (let testCase of [[0b0001, 1], [0b0010, 0], [0b0101, 1], [0b1010, 0]]) {
+for (let testCase of [[0b0001, 1], [0b0010, 0], [0b0101, 1], [0b1010, 0],
+    [0b0111, 1], [0b1111, 1], [0b0000, 0], [0b10100111, 1]]) {
   testLeastSignificantBit(testCase[0], testCase[1]);
 }
