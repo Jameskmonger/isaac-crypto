1459d5dd5863cbd1fbe577be2cf1107b0514efe9
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-02-04 13:41:54 +0000

    Added (incorrect) getMostSignificantBit method

--- src/bit-operations.ts
+++ src/bit-operations.ts
@@ -2,4 +2,8 @@ export class BitOperations {
   public static getLeastSignificantBit(input: number): number {
     return input & 0b1;
   }
+
+  public static getMostSignificantBit(input: number): number {
+    return 5;
+  }
 }
