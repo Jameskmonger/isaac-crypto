f189960f71ee60e8e26bbb5d3b15b9c0f1c6d2c4
James Monger <jameskmonger@hotmail.co.uk>
2016-02-05 00:00:06 +0000

    Create _count

--- src/isaac-generator.ts
+++ src/isaac-generator.ts
@@ -1,9 +1,15 @@
 export class IsaacGenerator {
+  private _count: number;
+
+  constructor() {
+    this._count = 0;
+  }
+
   public getValue(): number {
     return 0;
   }
 
   private _randomise(): void {
-    
+
   }
 }
