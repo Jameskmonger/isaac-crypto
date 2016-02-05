f408f106bec5003866ee4a7c13653aa8b9522a2e
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-02-05 16:40:04 +0000

    Use utils for initial values

--- test/_utils.ts
+++ test/_utils.ts
@@ -1,4 +1,17 @@
 export class Utils {
+  public static getInitialValues() {
+    return [
+      [ 'a', -3969392806 ],
+      [ 'b', -1780940711 ],
+      [ 'c', -1021952437 ],
+      [ 'd', 255990488 ],
+      [ 'e', -651539848 ],
+      [ 'f', -1525007287 ],
+      [ 'g', -990909925 ],
+      [ 'h', 811634969 ]
+    ];
+  }
+
   public static getSeedTestCases(seedValue: number) {
     console.log(`getting ${seedValue}`);
 
--- test/seed-provider.test.ts
+++ test/seed-provider.test.ts
@@ -33,16 +33,7 @@ let testValue = (key: string, expected: number) => {
   });
 };
 
-for (let testCase of [
-  [ 'a', -3969392806 ],
-  [ 'b', -1780940711 ],
-  [ 'c', -1021952437 ],
-  [ 'd', 255990488 ],
-  [ 'e', -651539848 ],
-  [ 'f', -1525007287 ],
-  [ 'g', -990909925 ],
-  [ 'h', 811634969 ]
-]) {
+for (let testCase of Utils.getInitialValues()) {
   testValue(testCase[0] as string, testCase[1] as number);
 }
 
