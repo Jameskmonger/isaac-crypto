88032ed1a4b972ca0652f3ccd48becb5cc01cff7
James Monger <jameskmonger@hotmail.co.uk>
2016-02-05 00:29:19 +0000

    Return correct values

--- src/seed-provider.ts
+++ src/seed-provider.ts
@@ -1,5 +1,14 @@
 export class SeedProvider {
   private _getInitialValues(): any {
-    return {};
+    return {
+      a: -3969392806,
+      b: -1780940711,
+      c: -1021952437,
+      d: 255990488,
+      e: -651539848,
+      f: -1525007287,
+      g: -990909925,
+      h: 811634969
+    };
   }
 }
