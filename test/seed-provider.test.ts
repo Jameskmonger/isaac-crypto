514aed028291a370f687c4bde265764fc397a98e
James Monger <jameskmonger@hotmail.co.uk>
2016-02-05 00:23:39 +0000

    Tests for a and b

--- test/seed-provider.test.ts
+++ test/seed-provider.test.ts
@@ -3,6 +3,15 @@
 import * as test from 'tape';
 import { SeedProvider } from '../src/seed-provider';
 
+test("_getInitialValues not undefined", (t) => {
+  let provider = new SeedProvider();
+
+  let values = provider["_getInitialValues"]();
+
+  t.notEqual(values, undefined, "values not undefined");
+  t.end();
+});
+
 test("_getInitialValues returns 8 values", (t) => {
   let provider = new SeedProvider();
 
@@ -11,3 +20,21 @@ test("_getInitialValues returns 8 values", (t) => {
   t.equal(values.length, 8, "8 initial values");
   t.end();
 });
+
+let testValue = (key: string, expected: number) => {
+  test(`_getInitialValues().${key} is ${expected}`, (t) => {
+    let provider = new SeedProvider();
+
+    let values = provider["_getInitialValues"]();
+
+    t.equal(values[key], expected, `correct value for ${key}`);
+    t.end();
+  });
+};
+
+for (let testCase of [
+  [ 'a', -3969392806 ],
+  [ 'b', -1780940711 ]
+]) {
+  testValue(testCase[0] as string, testCase[1] as number);
+}
