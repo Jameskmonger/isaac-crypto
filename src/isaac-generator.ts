c3e6c4dfa125198fddd50e84704026d73995846b
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-02-15 14:12:25 +0000

    Store required accumulator shifts (in order)

--- src/isaac-generator.ts
+++ src/isaac-generator.ts
@@ -2,10 +2,18 @@ import { SeedProvider } from './seed-provider';
 
 export class IsaacGenerator {
   private _count: number;
+  private _accumulatorShifts: Array<(x: number) => number>;
   private _memory: any;
 
   constructor(seed: Array<number>) {
     this._count = 0;
+
+    this._accumulatorShifts = [
+      (x: number) => x << 13,
+      (x: number) => x >>> 6,
+      (x: number) => x << 2,
+      (x: number) => x >>> 16
+    ];
 
     this._memory = SeedProvider.getMemory(seed);
   }
