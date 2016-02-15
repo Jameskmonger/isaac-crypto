d3da7a59b392fb82c1140294c9e311eba4793c76
Jameskmonger <jameskmonger@hotmail.co.uk>
2016-02-15 17:29:21 +0000

    Update constructor signature

--- test/isaac-generator.test.ts
+++ test/isaac-generator.test.ts
@@ -2,10 +2,11 @@
 
 import * as test from 'tape';
 import { IsaacGenerator } from '../src/isaac-generator';
+import { Utils } from './_utils';
 
 test("getValue calls _randomise if count is 0", (t) => {
   t.plan(1);
-  let generator = new IsaacGenerator();
+  let generator = new IsaacGenerator(Utils.createUniformSeed(0));
 
   generator["_randomise"] = () => {
     t.pass("_randomise called");
@@ -17,7 +18,7 @@ test("getValue calls _randomise if count is 0", (t) => {
 
 test("getValue calls _randomise if count is -1", (t) => {
   t.plan(1);
-  let generator = new IsaacGenerator();
+  let generator = new IsaacGenerator(Utils.createUniformSeed(0));
 
   generator["_randomise"] = () => {
     t.pass("_randomise called");
@@ -28,7 +29,7 @@ test("getValue calls _randomise if count is -1", (t) => {
 });
 
 test("getValue does not call _randomise if count is 1", (t) => {
-  let generator = new IsaacGenerator();
+  let generator = new IsaacGenerator(Utils.createUniformSeed(0));
   let _randomiseCalled = false;
   generator["_randomise"] = () => {
     _randomiseCalled = true;
